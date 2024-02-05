1df8cebc9d2a52d10bed2a71d0139b63b379c22e
ArinAkaMazu <arinkaushik60@gmail.com>
2024-02-05 19:51:15 +0530

    added comments

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pyttsx3
 import speech_recognition
+#----------------------------------------------------------------------------------------------------------
+#main engine
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[0].id)
-engine.setProperty("rate",170)
+engine.setProperty("voice", voices[0].id) #microsoft david voice
+engine.setProperty("rate",170) #audio sound
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
+#----------------------------------------------------------------------------------------------------------
 def takeCommand():
+#speech input command
     r=speech_recognition.Recognizer()
     with speech_recognition.Microphone() as source:
         print("listening.....")
-        r.pause_threshold=1
-        r.energy_threshold=300
-        audio=r.listen(source,0,4)
+        r.pause_threshold=1 #delay while speaking
+        r.energy_threshold=300 #energy consumed by user while giving input
+        audio=r.listen(source,0,4) #if no input given in 4 seconds goes to exception
     try:
         print("Processing...")
         query=r.recognize_google(audio,language="en-in")
@@ -31,13 +35,12 @@ if __name__=="__main__":
             from Greet import greet
             greet()
 #------------------------------------------------------------------------------------------------------
-    #speech values
+    #Greet values
             while True:
                 query= takeCommand().lower()
                 if "good night" in query:
-                    speak("Bye boss, wake me; when you need me")
-                    break
-    #shut down command
+                    speak("Bye boss, wake me; when you need me") #shut down command
+                    break #end of loop after saying "good night"/shutting down
                 elif "hello there" in query:
                     speak("Hi boss, how are you doing?")
                 elif "i am fine" in query:
@@ -48,10 +51,10 @@ if __name__=="__main__":
                     speak("Welcome, Boss")
 #------------------------------------------------------------------------------------------------------
                 elif "google" in query:
-                    from search import searchGoogle
+                    from search import searchGoogle #search input on google
                     searchGoogle(query)
                 elif "youtube" in query:
-                    from search import searchYoutube
+                    from search import searchYoutube #search input on youtube
                     searchYoutube(query)
                 
                 
