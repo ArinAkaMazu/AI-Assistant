df2cf24a4e1bdb40049930c4252ba426d12c9112
ArinAkaMazu <arinkaushik60@gmail.com>
2024-02-05 20:36:07 +0530

    comments

--- Greet.py
+++ Greet.py
@@ -5,11 +5,11 @@ engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[0].id)
 engine.setProperty("rate",170)
-
+#main engine from main.py
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-def greet():
+def greet(): 
     hour=int(datetime.datetime.now().hour)
     if hour>=0 and hour<=12:
         speak("Good Morning,boss")
