d8496f58368285afc023d66e865893ad4c804d47
ArinAkaMazu <arinkaushik60@gmail.com>
2024-02-29 15:21:48 +0530

    added background music

--- gui.py
+++ gui.py
@@ -1,33 +1,32 @@
 from tkinter import *
 from PIL import Image, ImageTk, ImageSequence
 import time
-import requests
 from io import BytesIO
+import requests
+import pygame
 from pygame import mixer
-
 mixer.init()
 root = Tk()
 root.geometry("1000x500")
-
-
-def play_gif():
+def play_gif(gif_url, music_file):
     root.lift()
     root.attributes("-topmost", True)
     global img
-    url = "https://media.giphy.com/media/doXBzUFJRxpaUbuaqz/giphy.gif"
-    response = requests.get(url)
+    response = requests.get(gif_url)
     img = Image.open(BytesIO(response.content))
     lbl = Label(root)
     lbl.place(x=0, y=0)
     i = 0
+    mixer.music.load(music_file)
+    mixer.music.play()
     for img_frame in ImageSequence.Iterator(img):
         img_frame = img_frame.resize((1000, 500))
         img_frame = ImageTk.PhotoImage(img_frame)
         lbl.config(image=img_frame)
         root.update()
-        time.sleep(0.05)
+        time.sleep(0.04)
     root.destroy()
-
-
-play_gif()
+gif_url = "https://media.giphy.com/media/doXBzUFJRxpaUbuaqz/giphy.gif"
+music_file = "bootsound.mp3"  
+play_gif(gif_url, music_file)
 root.mainloop()
