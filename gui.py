1026509b51545e8b34bdd4463768c52a0e9ee234
ArinAkaMazu <arinkaushik60@gmail.com>
2024-02-29 16:01:09 +0530

    updated gui gif and added music

--- gui.py
+++ gui.py
@@ -1,32 +1,28 @@
 from tkinter import *
 from PIL import Image, ImageTk, ImageSequence
 import time
-from io import BytesIO
-import requests
-import pygame
 from pygame import mixer
 mixer.init()
 root = Tk()
-root.geometry("1000x500")
-def play_gif(gif_url, music_file):
+root.attributes('-fullscreen', True)
+def play_gif():
     root.lift()
     root.attributes("-topmost", True)
     global img
-    response = requests.get(gif_url)
-    img = Image.open(BytesIO(response.content))
+    img = Image.open("giphy_s.gif")
     lbl = Label(root)
     lbl.place(x=0, y=0)
     i = 0
-    mixer.music.load(music_file)
+    mixer.music.load("bootsound.mp3")
     mixer.music.play()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
     for img_frame in ImageSequence.Iterator(img):
-        img_frame = img_frame.resize((1000, 500))
+        img_frame = img_frame.resize((screen_width, screen_height))
         img_frame = ImageTk.PhotoImage(img_frame)
         lbl.config(image=img_frame)
         root.update()
-        time.sleep(0.04)
+        time.sleep(0.05)
     root.destroy()
-gif_url = "https://media.giphy.com/media/doXBzUFJRxpaUbuaqz/giphy.gif"
-music_file = "bootsound.mp3"  
-play_gif(gif_url, music_file)
+play_gif()
 root.mainloop()
